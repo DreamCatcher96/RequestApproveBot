9812f9340e2d3ec5376e0989732dc9dfeee217f0
TEZZA <93602145+Tezzarobot@users.noreply.github.com>
2023-02-22 14:39:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,19 +37,20 @@ async def approve(_, m : Message):
     try:
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
-        img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @Galexycinemas__**".format(m.from_user.mention, m.chat.title))
-        add_user(kk.id)
-    except errors.PeerIdInvalid as e:
-        print("user isn't start bot(means group)")
-    except Exception as err:
-       keyboard = InlineKeyboardMarkup(
+        if m.approve.chat == enums.Autoapprove.JOINREQUEST:
+            keyboard = InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton("🎥 JOIN UPDATE CHANNEL 🎥", url="https://t.me/+MEj3eB0vNzEwNmE9"),    
                      ]
                 ]
             )
+        img = random.choice(gif)
+        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @Galexycinemas__**".format(m.from_user.mention, m.chat.title))
+        add_user(kk.id)
+    except errors.PeerIdInvalid as e:
+        print("user isn't start bot(means group)")
+    except Exception as err:
         print(str(err))
               
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Start ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
