c754b93553e712e5cc4e690670ecf830148d68b8
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2023-02-28 13:52:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ gif = [
     'https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4',
     'https://telegra.ph/file/b4834b434888de522fa49.mp4'
 ]
-
+key_btn = InlineKeyboardMarkup([[InlineKeyboardButton("UPDATE CHANNEL", url="https://t.me/galexycinemas")]])
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Main process ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
@@ -38,7 +38,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @Galexycinemas__**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @Galexycinemas__**".format(m.from_user.mention, m.chat.title),reply_markup=key_btn)
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
