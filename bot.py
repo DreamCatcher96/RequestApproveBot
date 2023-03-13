3156cd2394e9f6c5e181dddd41a1ba41e87694ca
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2023-03-13 14:51:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @Galexycinemas__**".format(m.from_user.mention, m.chat.title),reply_markup=key_btn)
+        await app.send_photo(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @Galexycinemas__**".format(m.from_user.mention, m.chat.title),reply_markup=key_btn)
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
